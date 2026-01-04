bbc7048057884a7181c6211662ff0f351450d22b
smycynek <sv@stevenvictor.net>
2026-01-04 16:36:02 -0500

    deploy update

--- solid/oh-snap/vite.config.ts
+++ solid/oh-snap/vite.config.ts
@@ -3,7 +3,7 @@ import solidPlugin from 'vite-plugin-solid';
 import devtools from 'solid-devtools/vite';
 
 export default defineConfig({
-  base: '/snap2/',
+  base: '/snap/',
   plugins: [devtools(), solidPlugin()],
   server: {
     port: 3000,
