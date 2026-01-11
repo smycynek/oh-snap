dbdc0f16d1843f5334804610fdf89bebecad88ae
smycynek <sv@stevenvictor.net>
2026-01-11 11:26:58 -0500

    offline mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 import devtools from 'solid-devtools/vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
   base: '/snap/',
-  plugins: [devtools(), solidPlugin()],
+  plugins: [devtools(), solidPlugin(), VitePWA({ registerType: 'autoUpdate' })],
   server: {
     port: 3000,
   },
